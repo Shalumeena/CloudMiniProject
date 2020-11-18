a087d37532171ab607c640cd36ac528b46a06a75
ShaluMeena <shalumeena.nagarathinam@gmail.com>
2020-11-18 19:53:03 +0530

    First Commit

---
+++ library/Controllers/ReturnBookController.cs
@@ -0,0 +1,62 @@
+﻿using library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace library.Controllers
+{
+    public class ReturnBookController : Controller
+    {
+        nlibraryEntities1 db = new nlibraryEntities1();
+        // GET: ReturnBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(returnbook retruns)
+        {
+            if (ModelState.IsValid)
+            {
+                db.returnbooks.Add(retruns);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(retruns);
+        }
+
+
+        public ActionResult GetMid(int mid)
+        {
+
+            var memberid = (from s in db.issuebooks
+                            where s.m_id == mid
+                            select new
+                            {
+                                IssueDate = s.issuedate,
+                                RetrunDate = s.returndate,
+                                Memberid = s.m_id,
+                                BookName = s.book_id,
+                                ElapsedDays = SqlFunctions.DateDiff("day", s.returndate, DateTime.Now)
+                            }).ToArray();
+
+            return Json(memberid, JsonRequestBehavior.AllowGet);
+        }
+
+     
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ library/Models/membervalidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace library.Models
+{
+    [MetadataType(typeof(memberMetaData))]
+    public partial class member
+    {
+        public class memberMetaData
+        {
+            [DisplayName("Member Name")]
+            public string name { get; set; }
+
+            [DisplayName("Address")]
+            public string address { get; set; }
+
+            [DisplayName("phone")]
+            public Nullable<int> phone { get; set; }
+        }
+    }
+}
